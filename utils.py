a5ca5027140f327f4b8233efc519e41e4d20b1b4
ereinha <74273926+ereinha@users.noreply.github.com>
2023-07-24 17:41:22 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -86,7 +86,7 @@ class DataLabelDataset(Dataset):
             self.data = data
             self.labels = labels
     def __getitem__(self, index):
-        return self.config[index], self.labels[index]
+        return self.data[index], self.labels[index]
 
     def __len__(self):
         return len(self.data)
