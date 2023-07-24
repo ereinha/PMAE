c80590d131e5efa673113417da29d9482f9ced1c
ereinha <74273926+ereinha@users.noreply.github.com>
2023-07-24 16:34:26 -0500

    Add files via upload

--- models/masks.py
+++ models/masks.py
@@ -41,3 +41,28 @@ class SpecificParticleMask(nn.Module):
             mask[b, self.particle, :] = 0
 
         return x * mask
+
+class KinematicMask(nn.Module):
+    def __init__(self, mask_count):
+        super(KinematicMask, self).__init__()
+        self.mask_count = mask_count
+
+    def forward(self, x):
+        assert x.dim() == 3, "Input tensor must be 3-dimensional (batch_size, seq_len, dim_in)"
+        batch_size, seq_len, _ = x.size()
+        assert self.mask_count <= seq_len, "Mask count must be less than or equal to the sequence length"
+
+        # Generate a mask tensor with the same shape as the input tensor
+        mask = torch.ones(batch_size, seq_len, 1, device=x.device)
+
+        # Generate a set of unique random indices to mask for each sample in the batch
+        for b in range(batch_size):
+            mask_indices = set()
+            while len(mask_indices) < self.mask_count:
+                mask_indices.add(random.randint(0, seq_len - 1))
+
+            # Zero out the elements at the selected indices
+            for idx in mask_indices:
+                mask[b, idx] = 0
+
+        return x * mask
