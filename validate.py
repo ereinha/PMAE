10e5d287946a97b2e183aa8176118d156fbba17d
ereinha <74273926+ereinha@users.noreply.github.com>
2023-07-24 17:26:03 -0500

    Add files via upload

--- validate.py
+++ validate.py
@@ -6,10 +6,10 @@ import json
 def validate(val_loader, models, device, criterion, model_type, output_vars, mask, epoch, num_epochs, val_loss_min, save_path, model_name):
     # Create a config checkpoint file
     config = parse_model_name(model_name)
-    with open('./outputs/' + model_name + '/ckpt_config.json', 'w') as f:
-        json.dump(config, f, indent=4)
 
     if model_type == 'autoencoder':
+        with open('./outputs/' + model_name + '/tae_ckpt_config.json', 'w') as f:
+            json.dump(config, f, indent=4)
         tae = models[0]
         tae.eval()  # Set the tae to evaluation mode
         losses = []
@@ -53,14 +53,16 @@ def validate(val_loader, models, device, criterion, model_type, output_vars, mas
             torch.save(tae.state_dict(), save_path + '/TAE_best_' + model_name)
 
         # Update the checkpoint file
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'r') as f:
+        with open('./outputs/' + model_name + '/tae_ckpt_config.json', 'r') as f:
             config = json.load(f)
         config['resume_epoch'] = epoch
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'w') as f:
+        with open('./outputs/' + model_name + '/tae_ckpt_config.json', 'w') as f:
             json.dump(config, f, indent=4)
         return val_loss_min, val_loss_min_2
 
     elif model_type == 'classifier partial':
+        with open('./outputs/' + model_name + '/partial_ckpt_config.json', 'w') as f:
+            json.dump(config, f, indent=4)
         tae, classifier = models[0], models[1]
         # Validation loop
         tae.eval()  # Set the tae to evaluation mode
@@ -104,14 +106,16 @@ def validate(val_loader, models, device, criterion, model_type, output_vars, mas
             torch.save(tae.state_dict(), save_path + '/Classifier_partial_best_' + model_name)
 
         # Update the checkpoint file
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'r') as f:
+        with open('./outputs/' + model_name + '/partial_ckpt_config.json', 'r') as f:
             config = json.load(f)
         config['resume_epoch'] = epoch
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'w') as f:
+        with open('./outputs/' + model_name + '/partial_ckpt_config.json', 'w') as f:
             json.dump(config, f, indent=4)
         return val_loss_min, val_loss_min_2
 
     elif model_type == 'classifier full':
+        with open('./outputs/' + model_name + '/full_ckpt_config.json', 'w') as f:
+            json.dump(config, f, indent=4)
         tae, classifier = models[0], models[1]
         # Validation loop
         tae.eval()  # Set the tae to evaluation mode
@@ -155,9 +159,9 @@ def validate(val_loader, models, device, criterion, model_type, output_vars, mas
             torch.save(tae.state_dict(), save_path + '/Classifier_full_best_' + model_name)
 
         # Update the checkpoint file
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'r') as f:
+        with open('./outputs/' + model_name + '/full_ckpt_config.json', 'r') as f:
             config = json.load(f)
         config['resume_epoch'] = epoch
-        with open('./outputs/' + model_name + '/ckpt_config.json', 'w') as f:
+        with open('./outputs/' + model_name + '/full_ckpt_config.json', 'w') as f:
             json.dump(config, f, indent=4)
         return val_loss_min, val_loss_min_2
